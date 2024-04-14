a5ef232bf1e272c459f4d0061bc092dd2cbdd4d9
Darren Soulier <56926037+Kichuwa@users.noreply.github.com>
2024-04-14 15:20:35 -0500

    Implemented ability to extract text into pdf output.

--- main.py
+++ main.py
@@ -5,11 +5,16 @@ from pathlib import Path
 filepaths = glob.glob("files/*txt")
 pdf = FPDF(orientation="P", unit="mm", format="A4")
 
+
 for path in filepaths:
     filename = Path(path).stem
     title = filename.title()
     pdf.add_page()
     pdf.set_font(family="Times", size=16, style="B")
     pdf.cell(w=50, h=8, txt=f"{title}", ln=1)
+    with open(path, "r") as text_output:
+        text_output = text_output.readline()
+        pdf.set_font(family="Times", size=12)
+        pdf.multi_cell(w=190, h=8, txt=f"{text_output}")
 
 pdf.output("animals.pdf")
